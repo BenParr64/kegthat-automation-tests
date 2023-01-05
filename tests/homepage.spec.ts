e29882c40cf84feb5ea4b2434bf07a3a6cdfb3bb
benparrtfl <benparr@tfl.gov.uk>
2023-01-05 16:57:32 +0000

    feat: add initial tests

---
+++ tests/homepage.spec.ts
@@ -0,0 +1,28 @@
+import { test, expect } from "@playwright/test";
+
+test("has title", async ({ page }) => {
+  await page.goto("https://kegthat-headless.vercel.app/");
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/Still Spirits Classic Flavours Archives/);
+});
+
+test("has info bar with dismiss button", async ({ page }) => {
+  await page.goto("https://kegthat-headless.vercel.app/");
+
+  await page.goto("https://kegthat.com/");
+
+  // Check if the container div element exists and contains the dismiss button
+  const container = page.getByText("Free postage over £75*");
+  const dismissButton = page.getByRole("button", { name: "DISMISS" });
+  expect(container).toBeTruthy();
+  expect(dismissButton).toBeTruthy();
+});
+
+test("has logo with alt text", async ({ page }) => {
+  await page.goto("https://kegthat-headless.vercel.app");
+
+  // Check if the logo element exists and has the correct alt text
+  const logo = page.getByRole("img", { name: "kegthat logo" });
+  expect(logo).toBeTruthy();
+});
